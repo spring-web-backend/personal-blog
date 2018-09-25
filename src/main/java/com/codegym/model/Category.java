e15f30b0f4cb47cf0f07b061dbbb5d187f5d9ea9
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-25 15:35:35 +0700

    create model for category

---
+++ src/main/java/com/codegym/model/Category.java
@@ -0,0 +1,37 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(targetEntity = Post.class)
+    private List<Post> posts;
+}
