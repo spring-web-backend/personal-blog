72134156e3447193bbc8c89a356e5010937cc8dd
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-25 16:04:26 +0700

    create category formatter

---
+++ src/main/java/com/codegym/formatter/CategoryFomatter.java
@@ -0,0 +1,30 @@
+package com.codegym.formatter;
+
+import com.codegym.model.Category;
+import com.codegym.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+public class CategoryFomatter implements Formatter<Category> {
+
+    @Autowired
+    CategoryService categoryService;
+
+    public CategoryFomatter(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+
+    @Override
+    public Category parse(String text, Locale locale) {
+        return categoryService.findById(Long.parseLong(text));
+    }
+
+    @Override
+    public String print(Category object, Locale locale) {
+        return "[" + object.getId() + ", " + object.getName() + "]";
+    }
+}
