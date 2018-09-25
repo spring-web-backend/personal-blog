c941d19a53493580ee3b5d540c5ae362bba5ba6f
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-25 16:19:49 +0700

    create post controller

---
+++ src/main/java/com/codegym/controller/PostController.java
@@ -0,0 +1,83 @@
+package com.codegym.controller;
+
+import com.codegym.model.Post;
+import com.codegym.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/post")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/create")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/post/create");
+        modelAndView.addObject("post", new Post());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView saveNewPost(@ModelAttribute("post") Post post) {
+        postService.save(post);
+
+        ModelAndView modelAndView = new ModelAndView("/post/create");
+        modelAndView.addObject("post", new Post());
+        modelAndView.addObject("msg", "New post has been created successfully!");
+        return modelAndView;
+    }
+
+    @GetMapping("")
+    public ModelAndView allPosts() {
+        Iterable<Post> posts = postService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/post/list");
+        modelAndView.addObject("posts", posts);
+        return modelAndView;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditForm(@PathVariable("id") Long id) {
+        Post post = postService.findById(id);
+        if (post != null) {
+            ModelAndView modelAndView = new ModelAndView("/post/edit");
+            modelAndView.addObject("post", post);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error-404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView updatePost(@ModelAttribute("post") Post post) {
+        postService.save(post);
+        ModelAndView modelAndView = new ModelAndView("/post/edit");
+        modelAndView.addObject("post", post);
+        modelAndView.addObject("msg", "Post has been updated!");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView showDeleteForm(@PathVariable("id") Long id) {
+        Post post = postService.findById(id);
+        if (post != null) {
+            ModelAndView modelAndView = new ModelAndView("/post/delete");
+            modelAndView.addObject("post", post);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error-404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/delete")
+    public String deletePost(@ModelAttribute("post") Post post) {
+        postService.remove(post.getId());
+        return "redirect:posts";
+    }
+
+}
