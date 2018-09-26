f98d8646c442c3524f74c478e12f13046f1d4a4d
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-26 11:12:04 +0700

    controller with post form

--- src/main/java/com/codegym/controller/PostController.java
+++ src/main/java/com/codegym/controller/PostController.java
@@ -2,12 +2,19 @@ package com.codegym.controller;
 
 import com.codegym.model.Category;
 import com.codegym.model.Post;
+import com.codegym.model.PostForm;
 import com.codegym.service.CategoryService;
 import com.codegym.service.PostService;
+import com.codegym.utils.StorageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/post")
@@ -19,6 +26,8 @@ public class PostController {
     @Autowired
     private CategoryService categoryService;
 
+    private Environment enviroment;
+
     @ModelAttribute("categories")
     public Iterable<Category> categories() {
         return categoryService.findAll();
@@ -27,16 +36,35 @@ public class PostController {
     @GetMapping("/create")
     public ModelAndView showCreateForm() {
         ModelAndView modelAndView = new ModelAndView("/post/create");
-        modelAndView.addObject("postForm", new Post());
+        modelAndView.addObject("postForm", new PostForm());
         return modelAndView;
     }
 
     @PostMapping("/create")
-    public ModelAndView saveNewPost(@ModelAttribute("post") Post post) {
+    public ModelAndView saveNewPost(@ModelAttribute("postForm") PostForm postForm) {
+        String randomFileName = "";
+        String originalFileName = postForm.getImage().getOriginalFilename();
+        if (!originalFileName.isEmpty()) {
+            randomFileName = StorageUtils.generateRandomFileName(originalFileName);
+            try {
+                postForm.getImage().transferTo(new File(enviroment.getProperty("image_location") + randomFileName));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Date now = new Date();
+        Post post = new Post(
+                postForm.getTitle(),
+                postForm.getDescription(),
+                postForm.getContent(),
+                randomFileName,
+                now,
+                postForm.getCategory()
+        );
         postService.save(post);
 
         ModelAndView modelAndView = new ModelAndView("/post/create");
-        modelAndView.addObject("post", new Post());
+        modelAndView.addObject("postForm", new PostForm());
         modelAndView.addObject("msg", "New post has been created successfully!");
         return modelAndView;
     }
@@ -52,22 +80,53 @@ public class PostController {
     @GetMapping("/edit/{id}")
     public ModelAndView showEditForm(@PathVariable("id") Long id) {
         Post post = postService.findById(id);
+
+        PostForm postForm = new PostForm();
+        postForm.setId(post.getId());
+        postForm.setTitle(post.getTitle());
+        postForm.setDescription(post.getDescription());
+        postForm.setContent(post.getContent());
+        postForm.setCategory(post.getCategory());
+        postForm.setImgUrl(post.getImgUrl());
+
+        ModelAndView modelAndView;
+
         if (post != null) {
-            ModelAndView modelAndView = new ModelAndView("/post/edit");
-            modelAndView.addObject("post", post);
-            return modelAndView;
+            modelAndView = new ModelAndView("/post/edit");
+            modelAndView.addObject("postForm", postForm);
         } else {
-            ModelAndView modelAndView = new ModelAndView("/error-404");
-            return modelAndView;
+            modelAndView = new ModelAndView("/error-404");
         }
+        return modelAndView;
     }
 
     @PostMapping("/edit")
-    public ModelAndView updatePost(@ModelAttribute("post") Post post) {
+    public ModelAndView updatePost(@ModelAttribute("postForm") PostForm postForm) {
+        Post post = new Post();
+
+        String originalFileName = postForm.getImage().getOriginalFilename();
+        if(!originalFileName.isEmpty()) {
+            try {
+                StorageUtils.removeImage(enviroment.getProperty("image_upload_location") + post.getImgUrl());
+                String randomFileName =  StorageUtils.generateRandomFileName(originalFileName);
+                postForm.getImage().transferTo(new File(enviroment.getProperty("image_upload_location") + randomFileName));
+                post.setImgUrl(randomFileName);
+                postForm.setImgUrl(randomFileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        post.setTitle(postForm.getTitle());
+        post.setDescription(postForm.getDescription());
+        post.setContent(postForm.getContent());
+        post.setPostedDate(new Date());
+        post.setCategory(postForm.getCategory());
         postService.save(post);
+
         ModelAndView modelAndView = new ModelAndView("/post/edit");
-        modelAndView.addObject("post", post);
-        modelAndView.addObject("msg", "Post has been updated!");
+        modelAndView.addObject("postForm", postForm);
+        modelAndView.addObject("msg", "Post has been updated");
         return modelAndView;
     }
 
